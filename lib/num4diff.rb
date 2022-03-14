abd96879a24d6c6ede3202a370ddafaabfcd1b16
siranovel <siranovel@gmail.com>
2022-03-14 10:44:17 +0900

    Add files via upload

--- lib/num4diff.rb
+++ lib/num4diff.rb
@@ -2,7 +2,16 @@ require 'ffi'
 
 module Num4DiffLib
     extend FFI::Library
-    ffi_lib ['libnum4diff.so', 'c']
-    attach_function :puts, [:string], :int
-    attach_function :eulerMethod, :CNum4Diff_eulerMethod, [:double, :double], :double
+    ffi_lib ['libnum4diff.so']
+    
+    callback   :f, [:double], :double
+    # yi_1 = eulerMethod(yi, x, h, func)
+    attach_function :eulerMethod,
+        :CNum4Diff_eulerMethod, [:double, :double, :double, :f], :double
+    # yi_1 = heunMethod(yi, x, h, func)
+    attach_function :heunMethod,
+        :CNum4Diff_heunMethod, [:double, :double, :double, :f], :double
+    # yi_1 = rungeKuttaMethod(yi, x, h, func)
+    attach_function :rungeKuttaMethod,
+        :CNum4Diff_rungeKuttaMethod, [:double, :double, :double, :f], :double
 end
