e3a20f53df995be01fb3e2316e579ccd44905b12
siranovel <siranovel@gmail.com>
2022-03-25 12:33:03 +0900

    Add files via upload

--- lib/num4diff.rb
+++ lib/num4diff.rb
@@ -5,15 +5,15 @@ module Num4DiffLib
     spec = Gem.loaded_specs["num4diff"]
     gem_root = spec.gem_dir
     ffi_lib ["#{gem_root}/ext/num4diff/libnum4diff.so"]
-    
+    # y = f(xi) 
     callback   :f, [:double], :double
-    # yi_1 = eulerMethod(yi, x, h, func)
+    # yi_1 = eulerMethod(yi, xi, h, func)
     attach_function :eulerMethod,
         :CNum4Diff_eulerMethod, [:double, :double, :double, :f], :double
-    # yi_1 = heunMethod(yi, x, h, func)
+    # yi_1 = heunMethod(yi, xi, h, func)
     attach_function :heunMethod,
         :CNum4Diff_heunMethod, [:double, :double, :double, :f], :double
-    # yi_1 = rungeKuttaMethod(yi, x, h, func)
+    # yi_1 = rungeKuttaMethod(yi, xi, h, func)
     attach_function :rungeKuttaMethod,
         :CNum4Diff_rungeKuttaMethod, [:double, :double, :double, :f], :double
 end
