c9b75ac07b80a6d6a208e78456fde0bec21dd5b5
siranovel <siranovel@gmail.com>
2023-04-24 13:16:27 +0900

    Add files via upload

--- ext/num4diff/CNum4Diff.h
+++ ext/num4diff/CNum4Diff.h
@@ -4,17 +4,27 @@
 /**************************************/
 /* 構造体宣言                         */
 /**************************************/
-typedef struct _CNum4Diff CNum4Diff;
+typedef struct _CNum4Diff           CNum4Diff;
+typedef struct _CNum4DiffTier       CNum4DiffTier;
+typedef struct _CNum4DiffMultistage CNum4DiffMultistage;
 typedef double (*Func)(double x);
 
-struct _CNum4Diff
+struct _CNum4DiffTier
 {
-	double (*FP_eulerMethod)(double yi, double xi, double h, Func func);
-	double (*FP_heunMethod)(double yi, double xi, double h, Func func);
-	double (*FP_rungeKuttaMethod)(double yi, double xi, double h, Func func);
+    double (*FP_eulerMethod)(double yi, double xi, double h, Func func);
+    double (*FP_heunMethod)(double yi, double xi, double h, Func func);
+    double (*FP_rungeKuttaMethod)(double yi, double xi, double h, Func func);
+};
+struct _CNum4DiffMultistage
+{
 	double (*FP_adamsBashforthMethod)(double a, double b, double y0, double h, Func func);
 	double (*FP_adamsMoultonMethod)(double a, double b, double y0, double h, Func func);
 ;
+};
+struct _CNum4Diff
+{
+    CNum4DiffTier tierMethod;
+    CNum4DiffMultistage multistageMethod;
 };
 /**************************************/
 /* define宣言                         */
@@ -22,9 +32,9 @@ struct _CNum4Diff
 /**************************************/
 /* プロトタイプ宣言                   */
 /**************************************/
-double CNum4Diff_eulerMethod(double yi, double xi, double h, Func func);
-double CNum4Diff_heunMethod(double yi, double xi, double h, Func func);
-double CNum4Diff_rungeKuttaMethod(double yi, double xi, double h, Func func);
-double CNum4Diff_adamsBashforthMethod(double a, double b, double y0, double h, Func func);
-double CNum4Diff_adamsMoultonMethod(double a, double b, double y0, double h, Func func);
+double CNum4Diff_Tier_eulerMethod(double yi, double xi, double h, Func func);
+double CNum4Diff_Tier_heunMethod(double yi, double xi, double h, Func func);
+double CNum4Diff_Tier_rungeKuttaMethod(double yi, double xi, double h, Func func);
+double CNum4Diff_Multistage_adamsBashforthMethod(double a, double b, double y0, double h, Func func);
+double CNum4Diff_Multistage_adamsMoultonMethod(double a, double b, double y0, double h, Func func);
 #endif
